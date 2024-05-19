ae9f12bf29e4739d1f8d8801d7cbc604228615b1
unknown <dejanmaric259@gmail.com>
2024-05-19 19:41:15 +0200

    init topbox ui

--- src/app/components/ListingPageTopBox/ListingPageTopBox.tsx
+++ src/app/components/ListingPageTopBox/ListingPageTopBox.tsx
@@ -2,6 +2,8 @@
 import { usePropertiesContext } from "@/app/contexts/PropertiesContext";
 import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const ListingPageTopBox = () => {
   const [details, setDetails] = useState<any>();
@@ -19,8 +21,53 @@ const ListingPageTopBox = () => {
     getDetails();
   }, [properties]);
 
+  if (!details) {
+    return <>Loading</>;
+  }
   return (
-    <section className="h-screen flex justify-center items-center gap-16 px-32"></section>
+    <section className="h-screen flex justify-center items-center gap-16 px-32 ">
+      <Image
+        className="rounded-lg w-full"
+        width={500}
+        height={500}
+        alt="TopBox Property"
+        src={details[0]?.image}
+      />
+      <div className="w-fit">
+        <h2 className="font-semibold text-4xl">{details[0]?.name}</h2>
+        <p className="text-lg text-gray-700 w-1/2">{details[0]?.description}</p>
+        <div className="flex flex-col gap-2 mt-8">
+          <div className="flex gap-2">
+            <Icon
+              icon="carbon:location-filled"
+              width="24"
+              height="24"
+              style={{ color: " #1f2937" }}
+            />
+            {details[0]?.address}
+          </div>
+          <div className="flex gap-2">
+            <Icon
+              icon="mdi:bed-outline"
+              width="24"
+              height="24"
+              style={{ color: " #1f2937" }}
+            />
+            {details[0]?.bedrooms}
+          </div>
+          <div className="flex gap-2">
+            <Icon
+              icon="iconoir:bathroom"
+              width="24"
+              height="24"
+              style={{ color: " #1f2937" }}
+            />
+            {details[0]?.bathrooms}
+          </div>
+        </div>
+        <h2 className="font-semibold text-4xl mt-8">${details[0]?.price}</h2>
+      </div>
+    </section>
   );
 };
 
