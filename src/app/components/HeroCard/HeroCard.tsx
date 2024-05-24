5a1c7b3a668e2cc05fbb5df304b25daed1e0366d
unknown <dejanmaric259@gmail.com>
2024-05-24 12:25:56 +0200

    hero section code cleanup

--- src/app/components/HeroCard/HeroCard.tsx
+++ src/app/components/HeroCard/HeroCard.tsx
@@ -8,38 +8,40 @@ import { usePropertiesContext } from "@/app/contexts/PropertiesContext";
 const HeroCard = () => {
   const { specialOffer } = usePropertiesContext();
 
-  if (!specialOffer) {
-    return;
+  if (!specialOffer || specialOffer.length === 0) {
+    return null;
   }
 
   return (
-    <div className="bg-gray-200 rounded-lg p-4 flex gap-4 w-full shadow-lg border border-gray-300 hover:scale-105 cursor-pointer z-40 relative">
-      <Image
-        className="rounded-lg"
-        width={360}
-        height={360}
-        alt="Hero Card Name"
-        src="https://images.unsplash.com/photo-1501183638710-841dd1904471?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg"
-      />
-      <div className="flex flex-col gap-2">
-        <h4 className="text-2xl font-semibold text-gray-950">
-          {specialOffer[0].name}
-        </h4>
-        <p>{specialOffer[0].description}</p>
-        <PropertyDetail
-          iconName="carbon:location-filled"
-          text={specialOffer[0].address}
+    specialOffer && (
+      <div className="bg-gray-200 rounded-lg p-4 flex gap-4 w-full shadow-lg border border-gray-300 hover:scale-105 cursor-pointer z-40 relative">
+        <Image
+          className="rounded-lg"
+          width={360}
+          height={360}
+          alt="Hero Card Name"
+          src={specialOffer[0].image}
         />
-        <PropertyDetail
-          iconName="iconoir:bathroom"
-          text={specialOffer[0].bathrooms}
-        />
-        <PropertyDetail
-          iconName="mdi:bed-outline"
-          text={specialOffer[0].bedrooms}
-        />
+        <div className="flex flex-col gap-2">
+          <h4 className="text-2xl font-semibold text-gray-950">
+            {specialOffer[0].name}
+          </h4>
+          <p>{specialOffer[0].description}</p>
+          <PropertyDetail
+            iconName="carbon:location-filled"
+            text={specialOffer[0].address}
+          />
+          <PropertyDetail
+            iconName="iconoir:bathroom"
+            text={specialOffer[0].bathrooms}
+          />
+          <PropertyDetail
+            iconName="mdi:bed-outline"
+            text={specialOffer[0].bedrooms}
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
