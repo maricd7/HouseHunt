be864caa194f0a5f5187a695aa9175012e5e1d81
unknown <dejanmaric259@gmail.com>
2024-05-24 20:22:57 +0200

    adding to wishlist button

--- src/app/components/ListingsHero/ListingCard/ListingCard.tsx
+++ src/app/components/ListingsHero/ListingCard/ListingCard.tsx
@@ -18,7 +18,7 @@ const ListingCard = ({
   return (
     <Link
       href={"/listings/" + id}
-      className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white p-4 w-72 cursor-pointer border border-gray-300 hover:scale-105 hover:border-blue-700 flex flex-col gap-4"
+      className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white p-4 w-72 cursor-pointer border border-gray-300 hover:scale-105 hover:border-blue-700 flex flex-col gap-4 relative"
     >
       <Image
         className="rounded-md h-36"
@@ -26,6 +26,13 @@ const ListingCard = ({
         height={400}
         alt="Listing"
         src={image}
+      />
+      <Icon
+        className="absolute top-6 right-6 hover text-white"
+        icon="carbon:location-heart-filled"
+        width="32"
+        height="32"
+        style={{ color: "#3b82f6" }}
       />
       <ListingCardMainInfo name={name} description={description} />
       <div className="flex flex-col gap-4">
