60f89baf6b405f5f455dcfc02a48f7030d8b65e5
unknown <dejanmaric259@gmail.com>
2024-06-08 18:33:58 +0200

    create listing component;

--- src/app/components/CreateListings/CreateListings.tsx
+++ src/app/components/CreateListings/CreateListings.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { Input } from "../common";
 
 const CreateListings = () => {
-  return <div>CreateListings</div>;
+  const propertyNameRef = useRef<HTMLInputElement>(null);
+  const propertyPriceRef = useRef<HTMLInputElement>(null);
+  const propertyDescriptionRef = useRef<HTMLInputElement>(null);
+  const propertyBathroomsRef = useRef<HTMLInputElement>(null);
+  const propertyBedroomsRef = useRef<HTMLInputElement>(null);
+  const propertyAddressRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form>
+      <Input
+        placeholder="Name of the Property"
+        label="Name"
+        type="text"
+        required
+        reference={propertyNameRef}
+      />
+      <Input
+        placeholder="Price of the Property"
+        label="Price"
+        type="number"
+        required
+        reference={propertyPriceRef}
+      />
+      <Input
+        placeholder="Description of the Property"
+        label="Description"
+        type="text"
+        required
+        reference={propertyDescriptionRef}
+      />
+      <Input
+        placeholder="Number of the Bathrooms"
+        label="Bathrooms"
+        type="number"
+        required
+        reference={propertyBathroomsRef}
+      />
+      <Input
+        placeholder="Number of the Bedrooms"
+        label="Bedrooms"
+        type="number"
+        required
+        reference={propertyBedroomsRef}
+      />
+      <Input
+        placeholder="Property Address"
+        label="Address"
+        type="text"
+        required
+        reference={propertyAddressRef}
+      />
+    </form>
+  );
 };
 
 export default CreateListings;
